d9cbe8b7113dc65e2442a1ed85d32d8e85c3516b
Pat F <tefl.pat@gmail.com>
2017-08-20 16:42:55 -0700

    Fix unit tests barfing on react-router links

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter as Router } from 'react-router-dom';
+
 import Header from './Header';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Header />, div);
+  ReactDOM.render(<Router><Header /></Router>, div);
 });
--- src/components/SidebarNav/SidebarNav.test.js
+++ src/components/SidebarNav/SidebarNav.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Sidebar from './Sidebar';
+import { MemoryRouter as Router } from 'react-router-dom';
+
+import SidebarNav from './SidebarNav';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Sidebar />, div);
+  ReactDOM.render(<Router><SidebarNav /></Router>, div);
 });
