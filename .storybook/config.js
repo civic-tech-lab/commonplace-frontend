9a22930328620f924f60e072ea33fdec1011022d
Andrew Hyndman <ajhyndman@hotmail.com>
2017-10-29 02:16:53 +1100

    Add stories for existing components

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,11 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
+  require('../src/components/App/demo.js');
+  require('../src/components/Footer/demo.js');
+  require('../src/components/Header/demo.js');
+  require('../src/components/Main/demo.js');
+  require('../src/components/Sidebar/demo.js');
   // You can require as many stories as you need.
 }
 
---
+++ src/components/Sidebar/demo.js
@@ -0,0 +1,7 @@
+// @flow
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Sidebar from './Sidebar';
+
+storiesOf('Sidebar', module).add('default', () => <Sidebar />);
