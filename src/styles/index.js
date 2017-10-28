cfbce16578c734291aaf095216c72f242f28c9e8
Andrew Hyndman <ajhyndman@hotmail.com>
2017-10-29 03:48:07 +1100

    Add project styles

---
+++ src/styles/index.js
@@ -0,0 +1,24 @@
+// @flow
+
+export const color = Object.freeze({
+  casper: '#B0B9D3',
+  cinnabar: '#E53333',
+});
+
+export const font = Object.freeze({
+  family: Object.freeze({
+    body: 'sans-serif',
+    heading: 'serif',
+    ui: 'sans-serif',
+  }),
+  size: Object.freeze({
+    large: 23,
+    medium: 15,
+  }),
+  weight: Object.freeze({
+    bold: 'bold',
+    normal: 'normal',
+  }),
+});
+
+export const transition = '.15s ease-in-out';
